42d6faf077dcce01da062ed4086a2ae552ddb79f
Guillermo Guerini <guillermo@gguerini.com>
2013-01-08 16:44:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leahandgui::Application.config.secret_token = 'e030cde87e75728dd8d56e9c148111e683656aadb7e31204b1ad03ac47cf7aeac4bba4a9dd89d5de6b634c8fb8dc0b7f5601b3d841455c31dbf31c6d39666731'
